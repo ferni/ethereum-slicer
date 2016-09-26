35e467cf7cfbaf5d891389a63073bbf78ade4729
Fernando Tiberti <fertiberti@gmail.com>
2016-09-26 01:08:16 -0300

    Implement default function

--- Slicer.sol
+++ Slicer.sol
@@ -1,22 +1,25 @@
 pragma solidity ^0.4.2;
 
 contract Slicer {
-    struct AddressDetails {
+    struct Slice {
         uint percentage;
-        uint etherBalance;
+        uint256 balance;
     }
-    mapping(address => AddressDetails) public slices;
+    mapping(address => Slice) public slices;
     address[] public beneficiaries;
+    uint firstBeneficiary = 0;
+    uint numberOfSlices;
     function Slicer(address[] _beneficiaries, uint[] _percentages) {
-        if (_beneficiaries.length > 10 || _beneficiaries.length == 0) {
+        numberOfSlices = _beneficiaries.length;
+        if (numberOfSlices > 10 || numberOfSlices == 0) {
             throw;
         }
-        if (_beneficiaries.length != _percentages.length) {
+        if (numberOfSlices != _percentages.length) {
             throw;
         }
         uint totalPercentage = 0;
-        for (var i = 0; i < _beneficiaries.length; i++) {
-            slices[_beneficiaries[i]] = AddressDetails(_percentages[i], 0);
+        for (var i = 0; i < numberOfSlices; i++) {
+            slices[_beneficiaries[i]] = Slice(_percentages[i] / 100, 0);
             totalPercentage += _percentages[i];
         }
         if (totalPercentage != 100) {
@@ -27,13 +30,37 @@ contract Slicer {
 
     modifier checkInvariants() {
         _;
-        uint totalEtherDistributed = 0;
-        for (var i = 0; i < beneficiaries.length; i++) {
-            totalEtherDistributed += slices[beneficiaries[i]].etherBalance;
+        uint totalWeiDistributed = 0;
+        for (var i = 0; i < numberOfSlices; i++) {
+            totalWeiDistributed += slices[beneficiaries[i]].balance;
+        }
+        if (totalWeiDistributed != this.balance) {
+            throw;
         }
     }
 
-    function () payable {
+    function() payable checkInvariants {
+        var index = firstBeneficiary;
+        uint256 amountLeft = msg.value;
 
+        //distribute to all minus one
+        for (var given = 0; given < numberOfSlices - 1; given++) {
+            var slice = slices[beneficiaries[index]];
+            uint256 toGive = msg.value * slice.percentage;
+            slice.balance += toGive;
+            amountLeft -= toGive;
+            index++;
+            if (index > numberOfSlices - 1) {
+                index = 0;
+            }
+        }
+        //the last one takes the rest
+        slices[beneficiaries[index]].balance += amountLeft;
+
+        //rotate beneficiaries so rounding errors matter less
+        firstBeneficiary++;
+        if (firstBeneficiary > numberOfSlices - 1) {
+            firstBeneficiary = 0;
+        }
     }
 }
